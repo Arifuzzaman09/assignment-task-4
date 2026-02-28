23b15a3b683b2d7f50beea376d7cca192f03a620
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-02-28 13:01:09 +0600

    compeleted all project

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ let rejectedContainer = document.getElementById('rejected-container')
 let totalCount = document.getElementById('total-count')
 let interviewCount = document.getElementById('interview-count')
 let rejectedCount = document.getElementById('rejected-count')
-
+let empty = document.getElementById('empty')
+let jobsCount = document.getElementById('jobs-count')
 
 
 function clickBtn(tab) {
     let tabs = ['all', 'interview', 'rejected']
-
+    allBtn = tab
     for (const t of tabs) {
         const tabName = document.getElementById('btn-' + t)
         if (t === tab) {
@@ -32,17 +33,30 @@ function clickBtn(tab) {
 
     for (const item of sectionItem) {
         item.classList.add('hidden')
+
     }
+    empty.classList.add('hidden')
+
     if (tab === 'all') {
         allContainer.classList.remove('hidden')
+        if (allContainer.children.length < 1) {
+            empty.classList.remove('hidden')
+        }
     }
     else if (tab === 'interview') {
         interviewContainer.classList.remove('hidden')
+        if (interviewContainer.children.length < 1) {
+            empty.classList.remove('hidden')
+        }
     }
     else {
         rejectedContainer.classList.remove('hidden')
-    }
+        if (rejectedContainer.children.length < 1) {
+            empty.classList.remove('hidden')
+        }
 
+    }
+    countStatus()
 }
 
 totalCount.innerText = allContainer.children.length
@@ -67,4 +81,27 @@ document.getElementById('main-container').addEventListener('click', function (ev
     if (targetAll.classList.contains('delete-btn')) {
         parent.removeChild(card)
     }
+    countStatus()
 })
+
+
+function countStatus() {
+   
+    const counts = {
+        all: allContainer.children.length,
+        interview: interviewContainer.children.length,
+        rejected: rejectedContainer.children.length
+    }
+    totalCount.innerText = counts.all;
+    interviewCount.innerText = counts.interview;
+    rejectedCount.innerText = counts.rejected;
+
+    jobsCount.innerText = counts[allBtn];
+
+    if (counts[allBtn] < 1) {
+        empty.classList.remove('hidden')
+    } else{
+        empty.classList.add('hidden')
+    }
+}
+countStatus()
